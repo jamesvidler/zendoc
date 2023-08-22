b5f477b53ae2c85fca8cf9c8c1eb9488ea4fadfd
jamesvidler <james.vidler@outlook.com>
2023-08-22 11:12:48 -0400

    stackpath spike

--- README.md
+++ README.md
@@ -1,35 +1,7 @@
 # Introduction 
-This package allows you to import an article into the Zendesk Help Center from a Markdown file using a command line. 
-
-## Why
-I wanted a way to generate release notes for our product automatically in a CI/D pipeline and then have it upload to Zendesk for review/tweaks before publishing it. 
-
-You could also use it to publish docs from a markdown github repository into zendesk Help Center.
+This package allows you to see which Stackpath sites in our stack are using the most bandwidth.
 
 
-# How to Use It
-Install `zendoc` globally so you can use it from the command-line:
-```
-npm install -g zendoc-cli
-```
-
-Run:
-```
-zendoc --token "xxxxxxxxxxxxxxxxxxxxx" --user  james@xxxxxxxx.com --title "My article title" --section_id 360003512451 --permission_group_id 1364252
-```
-
-## Parameters
-`--token` -> [string] The API token from zendesk (required)
-
-`--user` -> [string] Your zendesk username (required)
-
-`--title` -> [string] The title for the article you want to import (required)
-
-`--section_id` -> [int] The section_id for the article (required)
-
-`--permission_group_id` -> [int] The permission_group_id for the article, i.e. the id for *Managers* or *Agents and Managers* (required)
-
-`--draft` -> [bool] Default is **true**, if set to **false** then the article will be published (optional)
 
 
 
--- cli.js
+++ cli.js
@@ -11,111 +11,144 @@ var warn = clc.yellow;
 var notice = clc.blue;
 
 //validate required args
-if(!argv.user) {
-    console.error(error('Missing --user argument. This is Zendesk username/email.'));
+if(!argv.client_id) {
+    console.error(error('Missing --client_id argument. This is your Stackpath client ID for the API.'));
     return;
 }
 
-if(argv.token === undefined) {
-    console.error(error('Missing --token argument. This is your API token for Zendesk.'));
+if(argv.client_secret === undefined) {
+    console.error(error('Missing --client_secret argument. This is your Stackpath cilent secret for the API.'));
     return;
 }
 
-if(argv.title === undefined) {
-    console.error(error('Missing --title argument. Title is the title of the article you want to import.'));
+if(argv.start_date === undefined) {
+    console.error(error('Missing --start_date argument. Example: 2020-11-15T00:00:00Z.'));
     return;
 }
 
-if(argv.section_id === undefined) {
-    console.error(error('Missing --section_id argument. Title is the sectionId of the article you want to import to.'));
+if(argv.end_date === undefined) {
+    console.error(error('Missing --end_date argument. Example: 2020-11-30T00:00:00Z'));
     return;
 }
 
-if(argv.permission_group_id === undefined) {
-    console.error(error('Missing --permission_group_id argument. This is Id of the permission group that can manage the article (i.e. Manager or Agents/Managers).'));
-    return;
+if(argv.stack_id === undefined) {
+    console.error(error('Missing --stack_id argument. Example: agilitycms-cloud-sites-d2caf6'))
 }
 
-if(argv.file === undefined) {
-    console.error(warn('Missing --file argument. The script will continue and assume you have a file located at ./content.md.'));
-}
-
-if(argv.draft === undefined || argv.draft === true) {
-    console.log("Article will be created as Draft (default behaviour). Pass-in '--draft false' to create and publish. ");
-}
 
 //default options
 var options = {
-    subdomain: 'agilitycms',
-    user: null, //passed in from cmd
-    token: null, //passed in from cmd
-    section_id: -1, //passed in from cmd,
-    title: null, //passed in from cmd
-    file: './content.md', //assume a file called content.md unless otherwise passed-in
-    draft: true,
-    user_segment_id: null,
-    permission_group_id: null //passed in from cmd
+    client_id: null,
+    client_secret: null,
+    start_date: null,
+    end_date: null,
+    stack_id: null
 }
 
 //overwrite defaults
 options = {...options, ...argv};
 
-if(options.draft) {
-    console.log(notice("Article will be published..."))
-}
-
-//GO!
-var go = function(article) {
-    fs.readFile(options.file, 'utf8', function(err, data) {
-        if (err) throw err;
-        
-        var html = marked(data);
-
-        article.body = html;
-    
-        createArticle(article, function(error, response, body) {
-            if(error) console.error(error);
-            if(response && response.statusCode) console.log(response.statusCode);
-            console.log(body);
-        });
-    
-        
-    });
-}
-
-
-var buildAuthHeader = function() {
-    return {
-        'user': `${options.user}/token`,
-        'pass': options.token,
-        'sendImmediately': true
-       };
-}
-
-var createArticle = function(article, cb) {
-    
-    var data = {
-        article: article
-    };
+var getAuthToken = function(cb) {
     request({
         method: 'POST',
-        uri: `https://${options.subdomain}.zendesk.com/api/v2/help_center/sections/${options.section_id}/articles.json`,
-        body: data,
+        uri: `https://gateway.stackpath.com/identity/v1/oauth2/token`,
+        body: {
+            grant_type: 'client_credentials',
+            client_id: options.client_id,
+            client_secret: options.client_secret
+        },
         json: true,
-        auth: buildAuthHeader()
     }, function(error, response, body) {
-        cb(error, response, body);
+        if(error) {
+            console.log(error(error));
+            return;
+        } 
+        if(response && response.statusCode) {
+            cb(body.access_token);
+        }
     });
 }
 
-go({ 
-    title: options.title,
-    body: '', //is set after markdown file is converted to html
-    locale: 'en-us',
-    user_segment_id: options.user_segment_id,
-    permission_group_id: options.permission_group_id,
-    draft: options.draft
-});
+var getMetrics = function(processMetrics) {
+    getAuthToken(function(access_token) {
+        //make the request to get metrics
+        request({
+            method: 'GET',
+            uri: `https://gateway.stackpath.com/delivery/v1/stacks/${options.stack_id}/metrics?metric_type=TRANSFER&granularity=P1M&platforms=CDE&group_by=SITE&start_date=${options.start_date}&end_date=${options.end_date}`,
+            body: data,
+            json: true,
+            auth: {
+                bearer: access_token
+            }
+        }, function(error, response, body) {
+            if(error) {
+                console.log(error(error));
+                return;
+            } 
+            if(response && response.statusCode) {
+                processMetrics(body);
+            }
+        });
+    })
+    
+}
+
+getMetrics(function(response) {
+    console.log(response);
+})
+
+//GO!
+// var go = function(article) {
+//     fs.readFile(options.file, 'utf8', function(err, data) {
+//         if (err) throw err;
+        
+//         var html = marked(data);
+
+//         article.body = html;
+    
+//         createArticle(article, function(error, response, body) {
+//             if(error) console.error(error);
+//             if(response && response.statusCode) console.log(response.statusCode);
+//             console.log(body);
+//         });
+    
+        
+//     });
+// }
+
+
+// var buildAuthHeader = function() {
+//     return {
+//         'user': `${options.user}/token`,
+//         'pass': options.token,
+//         'sendImmediately': true
+//        };
+// }
+
+// var createArticle = function(article, cb) {
+    
+//     var data = {
+//         article: article
+//     };
+//     request({
+//         method: 'POST',
+//         uri: `https://${options.subdomain}.zendesk.com/api/v2/help_center/sections/${options.section_id}/articles.json`,
+//         body: data,
+//         json: true,
+//         auth: buildAuthHeader()
+//     }, function(error, response, body) {
+//         cb(error, response, body);
+//     });
+// }
+
+// go({ 
+//     title: options.title,
+//     body: '', //is set after markdown file is converted to html
+//     locale: 'en-us',
+//     user_segment_id: options.user_segment_id,
+//     permission_group_id: options.permission_group_id,
+//     draft: options.draft
+// });
 
 
 
